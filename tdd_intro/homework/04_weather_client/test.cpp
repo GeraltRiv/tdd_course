13e0fc398535bda474561c6bc2c1521c9804fa2c
Krasnik Evnheniy <krasnik.evheniy@apriorit.com>
2019-06-24 13:44:24 +0300

    Green test

--- tdd_intro/homework/04_weather_client/test.cpp
+++ tdd_intro/homework/04_weather_client/test.cpp
@@ -63,6 +63,8 @@ IMPORTANT:
 // 1. Send 4 day times, receive response, calculate windSpeed
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <iostream>
+#include <map>
 
 class IWeatherServer
 {
@@ -72,10 +74,25 @@ public:
     virtual std::string GetWeather(const std::string& request) = 0;
 };
 
+//"31.08.2018;09:00" : "23;204;4.9"
+//"31.08.2018;15:00" : "33;193;4.3"
+//"31.08.2018;21:00" : "26;179;4.5"
+
+//"01.09.2018;03:00" : "19;176;4.2"
+//"01.09.2018;09:00" : "22;131;4.1"
+//"01.09.2018;15:00" : "31;109;4.0"
+//"01.09.2018;21:00" : "24;127;4.1"
+
+//"02.09.2018;03:00" : "21;158;3.8"
+//"02.09.2018;09:00" : "25;201;3.5"
+//"02.09.2018;15:00" : "34;258;3.7"
+//"02.09.2018;21:00" : "27;299;4.0"
 class MockWeatherServer : public IWeatherServer{
+    std::map<std::string, std::string> serverMap = {{"31.08.2018;03:00", "20;181;5.1"}, {"31.08.2018;15:00", "33;193;4.3"},
+                                                    {"31.08.2018;21:00", "26;179;4.5"}, {"01.09.2018;03:00", "19;176;4.2"}};
 
     std::string GetWeather(const std::string& request) override {
-        return "";
+        return serverMap[request];
     }
 };
 
